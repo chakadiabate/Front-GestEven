35eee2b7285a8ad75fa3d4dfa5973eb1f5229762
bouba8307 <boubacartraore8307@gmail.com>
2024-07-31 11:35:08 +0000

    mycommit 11:30

--- src/main/java/com/kalanso/event/Controller/PrestateurController.java
+++ src/main/java/com/kalanso/event/Controller/PrestateurController.java
@@ -1,44 +1,71 @@
 package com.kalanso.event.Controller;
 
-import com.kalanso.event.Model.Presta;
+import com.kalanso.event.Model.Prestateur;
 import com.kalanso.event.Service.PrestateurService;
-import lombok.AllArgsConstructor;
+import com.kalanso.event.Service.Utilisateur_service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
 @CrossOrigin(origins="*")
 @RestController
-@RequestMapping("/api/prestateurs")
-@AllArgsConstructor
+@RequestMapping("/gestEvent/prestateurs")
+
 public class PrestateurController {
-   @Autowired
+
+    @Autowired
     private PrestateurService prestateurService;
+    private Utilisateur_service utilisateurService;
 
-    @GetMapping("/ListePresta")
-    public List<Presta> getAllPrestateurs() {
-        return prestateurService.ListPrestateurs();
+    @GetMapping
+    public ResponseEntity<List<Prestateur>> getAllPrestateurs() {
+        List<Prestateur> prestateurs = prestateurService.findAll();
+        return ResponseEntity.ok(prestateurs);
     }
 
-
-
-    @PostMapping("/CreerPresta")
-    public Presta createPrestateur(@RequestBody Presta prestateur) {
-        return prestateurService.AjouterPresta(prestateur);
+    @GetMapping("/{id}")
+    public ResponseEntity<Prestateur> getPrestateurById(@PathVariable Long id) {
+        Optional<Prestateur> prestateur = prestateurService.findById(id);
+        return prestateur.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/ModifPresta/{id}")
-       public Presta ModifPresta(@PathVariable Long id, @RequestBody Presta presta)
-    {
-        return prestateurService.ModifPresta(id, presta);
+    @PostMapping
+    public ResponseEntity<Prestateur> createPrestateur( @RequestBody Prestateur prestateur) {
+        Prestateur savedPrestateur = prestateurService.save(prestateur);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPrestateur);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Prestateur> updatePrestateur(@PathVariable Long id, @RequestBody Prestateur prestateurDetails) {
+        Optional<Prestateur> prestateur = prestateurService.findById(id);
 
-    @DeleteMapping("/delete/{id}")
-    public String deletePrestateur(@PathVariable Long id)
-    {
-        return prestateurService.SupPrestateur(id);
+        if (prestateur.isPresent()) {
+            Prestateur prestateurToUpdate = prestateur.get();
+            prestateurToUpdate.setNom(prestateurDetails.getNom());
+            prestateurToUpdate.setEmail(prestateurDetails.getEmail());
+            prestateurToUpdate.setProfile(prestateurDetails.getProfile());
+            prestateurToUpdate.setTelephone(prestateurDetails.getTelephone());
+            prestateurToUpdate.setDescription(prestateurDetails.getDescription());
+            Prestateur updatedPrestateur = prestateurService.save(prestateurToUpdate);
+            return ResponseEntity.ok(updatedPrestateur);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePrestateur(@PathVariable Long id) {
+        Optional<Prestateur> prestateur = prestateurService.findById(id);
+
+        if (prestateur.isPresent()) {
+            prestateurService.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
